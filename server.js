509903c1196dbc03b1d1d6de03adfa3cd87f0de7
Tenzin Ngawang <tenzin.ngawang0@gmail.com>
2019-09-18 10:59:20 -0400

    pre deployment changes to heroku

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ const POKEDEX = require('./pokedex.json');
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiy' : 'common'
+app.use(morgan(morganSetting));
 app.use(helmet())
 app.use(cors())
+
 
 const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`,
   `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`,
@@ -56,9 +58,16 @@ app.get('/pokemon', function handleGetPokemon(req, res) {
   res.json(response)
 })
 
-
-const PORT = 8000
-
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: {message: 'server error'}}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
 })
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT)
