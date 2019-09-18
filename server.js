c7d9be6d4515bdd9de562a6327340cb41d63df1b
Tenzin Ngawang <tenzin.ngawang0@gmail.com>
2019-09-18 10:31:20 -0400

    deploy on local port, switching to Heroku server after this commit

--- server.js
+++ server.js
@@ -1,13 +1,61 @@
+require('dotenv').config();
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const POKEDEX = require('./pokedex.json');
+
 
 const app = express()
 
 app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
 
-app.use((req, res) => {
-  res.send('I am hungry!')
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`,
+  `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`,
+  `Psychic`, `Rock`, `Steel`, `Water`]
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  next();
 })
+
+app.get('/types', function handleGetTypes(req, res) {
+  res.json(validTypes)
+})
+
+
+app.get('/pokemon', function handleGetPokemon(req, res) {
+  // res.send('Hello, pokemon')
+  const { name } = req.query;
+  const { type } = req.query;
+  //case insensitive 
+  let response = POKEDEX.pokemon;
+
+  if (name) {
+    response = response
+      .filter(pokemon =>
+        pokemon
+          .name
+          .toLowerCase()
+          .includes(name.toLowerCase()));
+  }
+
+  if (type) {
+    response = response.filter(pokemon =>
+      pokemon.type.includes(type)
+    )
+  }
+
+  res.json(response)
+})
+
 
 const PORT = 8000
 
